00ba8a5d91b6cd826a5554dcfffbdac3b581e825
alanchung0216 <alanchung0216@gmail.com>
2016-02-21 15:09:09 -0500

    this is second output

--- Git_project_1_win/src/com/acproject/GitTest.java
+++ Git_project_1_win/src/com/acproject/GitTest.java
@@ -3,6 +3,7 @@ package com.acproject;
 public class GitTest {
 
 	public static void main(String[] args) {
-			System.out.println("this is first output");
+		System.out.println("this is first output");
+		System.out.println("this is second output");
 	}
 }
